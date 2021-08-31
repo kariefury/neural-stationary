e4db000f3217c16951a804e2132b7aa366785414
root <carrie@skyberrys.com>
2021-08-31 04:55:59 +0000

    plotting

--- run_exp.sh
+++ run_exp.sh
@@ -1,2 +1,2 @@
 ngspice PreLayout/pulsegate3_5.cir
-gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -dGraphicsAlphaBits=4 -r300 -sOutputFile=plot.png plot.ps_
+gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -dGraphicsAlphaBits=4 -r300 -sOutputFile=plotnoise.png plotnoise.ps
