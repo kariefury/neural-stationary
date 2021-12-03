7fab96d2c7670284074dd9a1dfc5df54d3198b6e
Carrie Segal <chsegal@umail.ucsb.edu>
2021-12-03 11:41:44 -0800

    cleaning and organizing

---
+++ multiple_sources/spice_file_maker_8input5p7nSeries_12Sources.py
@@ -0,0 +1,87 @@
+
+header = "*PulseLoop\n \
+.include sky130nm.lib\n \
+Xpg sNoise1 sNoise2 sNoise3 sNoise4 sNoise5 sNoise6 sNoise7 sNoise8 sNoise9 sNoise10 sNoise11 sNoise12 out pg\n \
+.measure tran responseTime WHEN v(out)=1.2 CROSS=1\n \
+.measure tran secondrTime WHEN v(out)=1.2 CROSS=2\n"
+
+footer = ".control \n" 
+
+
+footer2 = "*quit\n \
+.endc\n \
+\n \
+\n \
+*PG\n \
+.subckt pg A B C D E F G H I J K L x\n \
+\n \
+xm1 1 reset_loop critical_node 1 sky130_fd_pr__pfet_01v8 l=150n w=720n\n \
+\n \
+xm16 0 H Ha 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+xm15 Ha G Ga 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+xm14 Ga F Fa 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+xm13 Fa E Ea 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+xm11 Ea D Da 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+xm12 Da C Ca 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+xm2 Ca A critical_node 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+xm17 Cb B critical_node 1 sky130_fd_pr__pfet_01v8 l=150n w=720n\n \
+xm18 Db I Cb 1 sky130_fd_pr__pfet_01v8 l=150n w=720n\n \
+xm19 Eb J Db 1 sky130_fd_pr__pfet_01v8 l=150n w=720n\n \
+xm20 Fb K Eb 1 sky130_fd_pr__pfet_01v8 l=150n w=720n\n \
+xm21 1 L Fb 1 sky130_fd_pr__pfet_01v8 l=150n w=720n\n \
+\n \
+xm3 1 critical_node invO 1 sky130_fd_pr__pfet_01v8 l=150n w=720n\n \
+xm4 0 critical_node invO 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+\n \
+xm4 1 invO reset_loop 1 sky130_fd_pr__pfet_01v8 l=150n w=720n\n \
+xm5 0 invO reset_loop 0 sky130_fd_pr__nfet_01v8 l=150n w=360n\n \
+\n \
+xm6 1 invO xe 1 sky130_fd_pr__pfet_01v8 l=150n w=720n \n \
+xm7 0 invO xe 0 sky130_fd_pr__nfet_01v8 l=150n w=360n \n \
+\n \
+xm8 1 xe x 1 sky130_fd_pr__pfet_01v8 l=150n w=720n \n \
+xm9 0 xe x 0 sky130_fd_pr__nfet_01v8 l=150n w=360n \n \
+\n \
+\n \
+v1 1 0 1.8 \n \
+\n \
+.ends pg \n \
+"
+
+filenames = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r"]
+
+std_dev = 0.1
+voltage = 0.1
+time = 16.5
+for n in filenames:
+    name_o_file = "PreLayout/noise_pulsegate12in_5p7nSeries12Sources"+n+".cir"
+    f = open(name_o_file,"w")
+    f.write(header)
+    f.write("v2 sNoise1 0 dc 0 trrandom (2 20p 0 "+str(std_dev)+ " " + str(voltage) + ")\n")
+    f.write("v3 sNoise2 0 dc 0 trrandom (2 20p 0 "+str(std_dev)+ " " + str(voltage) + ")\n")
+    f.write("v4 sNoise3 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v5 sNoise4 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v6 sNoise5 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v7 sNoise6 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v8 sNoise7 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v9 sNoise8 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v10 sNoise9 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v11 sNoise10 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v12 sNoise11 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    f.write("v13 sNoise12 0 dc 0 trrandom (2 20p 0 " + str(std_dev) + " " + str(voltage) + ")\n")
+    std_dev += 0.1
+    f.write(footer)
+    #f.write("hardcopy plot1"+n+" v(out)+2 v(sNoise) \n")
+    f.write("tran 20ps " +str(time)+"ns \n ")
+    f.write(footer2)
+    time = time - 0.5
+    
+name_o_sh = "run_exp_noise_first_event_time_12input5p7nSeries12Sources.sh"
+f = open(name_o_sh,"w")
+n = 0
+for a in filenames:
+    n = 0
+    while n < 50:
+        f.write("ngspice -b -o noiseFirstStart5p7nSeries12Sources/data" +str(n) + a + ".txt "
+                                                                        "PreLayout/noise_pulsegate12in_5p7nSeries12Sources"+a+".cir\n")
+        n += 1
