c82d20ede5c1084a190e1ea09e2c910134c337c2
Carrie Segal <chsegal@umail.ucsb.edu>
2022-01-18 12:38:19 -0800

    updated plot for start up time with multiple series inputs

--- experiment_outline.py
+++ experiment_outline.py
@@ -4,39 +4,60 @@ import numpy as np
 import nengo
 from nengo.processes import WhiteSignal
 from nengo.solvers import LstsqL2
+from nengo.dists import Uniform
 
-model = nengo.Network()
+reseed = 252525
+model = nengo.Network(label="Lower Bound Network", seed=reseed)
 
 num_n = 1
-time_to_run = 10.0
 
+white_gaussian_process = nengo.processes.WhiteNoise(dist=nengo.dists.Gaussian(0.01, 0.1),scale=True)
+
+
+ntr = 5e-5# Uniform(1e-2, 4e-7).sample(1)[0]
+ntrc = 2e-5#Uniform(1e-2, 4e-7).sample(1)[0]
+sim = nengo.Simulator(model,dt=1e-9)
+
+time_to_run = ntr*50
+white_gaussian_process.trange(time_to_run,dt=1e-9)
+
+pt = sim.dt
 with model:
     # a Node provides non-neural inputs to the model
-    input_signal = nengo.Node(WhiteSignal(num_n, high=5), size_out=1)
+    input_signal = nengo.Node(white_gaussian_process, size_out=1)
     # A population of neurons, counted to num_n
-    pre = nengo.Ensemble(num_n, dimensions=1)
+    pre = nengo.Ensemble(num_n, 
+                        dimensions=1,
+                        neuron_type = nengo.LIF(min_voltage=0, tau_ref=ntr, tau_rc=ntrc),  # Specify type of neuron)
+                        max_rates = Uniform(1 / (ntr + sim.dt), 1 / (ntr + sim.dt))) 
+
     # A unidirectional connection between two objects (x,y) x connects to y. y does not connect to x
     # A connection can use a synapse model to include a filter, 
-    # otherwise the information will be transmitted without delay in backends that support that.
+    # otherwise the information will be transmitted without delay, in backends that support that.
     #input_connection = \
-    nengo.Connection(input_signal, pre)
+    nengo.Connection(input_signal, pre, synapse=0)
 
     post = nengo.Ensemble(num_n, dimensions=1)
-    conn = nengo.Connection(pre, post, function=lambda x: np.random.random(1))
-    input_signal_probe = nengo.Probe(input_signal)
-    pre_p = nengo.Probe(pre, synapse=0.01)
-    post_p = nengo.Probe(post, synapse=0.01)
+    conn = nengo.Connection(pre, post,synapse=0)
+    input_signal_probe = nengo.Probe(input_signal, synapse=0.005)
+    pre_p = nengo.Probe(pre, synapse=0)
+    post_p = nengo.Probe(post, synapse=0.001)
     #pre_p = nengo.Probe(pre)
     #post_p = nengo.Probe(post)
 
-with nengo.Simulator(model) as sim:
+with nengo.Simulator(model,dt=1e-9) as sim:
     sim.run(time_to_run)
 
 plt.figure(figsize=(8, 8))
 #plt.subplot(2, 1, 1)
-plt.plot(sim.trange(), sim.data[input_signal_probe].T[0], c="k", label="Input")
-plt.plot(sim.trange(), sim.data[pre_p].T[0], c="b", label="Pre")
-plt.plot(sim.trange(), sim.data[post_p].T[0], c="r", label="Post")
+start_plot = 0
+end_plot = len(sim.trange())
+print(len(sim.trange()))
+print(len(sim.data[input_signal_probe].T[0]))
+plt.plot(sim.trange()[start_plot:end_plot], sim.data[input_signal_probe].T[0][start_plot:end_plot], c="k", label="Input")
+plt.plot(sim.trange()[start_plot:end_plot], sim.data[pre_p].T[0][start_plot:end_plot], c="b", label="Presynaptic "
+                                                                                                    "Output")
+#plt.plot(sim.trange()[500:end_plot], sim.data[post_p].T[0][500:end_plot], c="r", label="Post")
 plt.ylabel("Dimension 1")
 plt.legend(loc="best")
 plt.legend(loc="best")
--- multiple_sources/read_first_events.py
+++ multiple_sources/read_first_events.py
@@ -5,13 +5,15 @@ paths = ['noiseFirstStart/','noiseFirstStart1p3nSeries/','noiseFirstStart1p4nSer
 		 'noiseFirstStart1p4nSeries2Sources/',\
 		'noiseFirstStart1p7nSeries8Sources/','noiseFirstStart5p7nSeries12Sources/',
 		 'noiseFirstStart8p8nSeries16Sources/']
+
+flabels = ['1n','1p3n','1p4n','1p4n','1p7n','5p7n','8p8n']
 clab = ['black','blue','green','yellow','magenta','cyan','grey']
 
 
 filenames = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r"]
 label = {"a":0.1, "b":0.2,"c":0.3,"d":0.4,"e":0.5,"f":0.6,"g":0.7,"h":0.8,"i":0.9,"j":1.0,"k":1.1,"l":1.2,"m":1.3,
 		 "n":1.4,"o":1.5,"p":1.6 ,"q":1.7 ,"r":1.8 }
-
+dpi = 300
 x = []
 y = []
 colors = []
@@ -52,6 +54,7 @@ import matplotlib.pyplot as plt
 i = 0
 
 #'black','blue','green','yellow','magenta','cyan','grey'
+plt.figure(figsize=(8, 5))
 line_choices=['-k','-b','-g','-y','m--','c--','g--']
 for each in paths:
 	# fit least-squares with an intercept
@@ -69,7 +72,7 @@ for each in paths:
 	# print(xnp.shape,'xnp')
 	# #print(len(x[i]),len(yc))
 	# #print(yc,x[i])
-	plt.scatter(xnp, ynp, c=colors[i], label=paths[i])
+	plt.scatter(xnp, ynp, c=colors[i], label=flabels[i])
 	# hs = np.hstack((xnp, ynp))
 	# print(hs.shape,ynp.shape)
 	# res = np.linalg.lstsq(hs, ynp)
@@ -80,11 +83,13 @@ for each in paths:
 	# xx = np.linspace(*plt.gca().get_xlim()).T
 	xj = xnp
 	yj = ynp
-	plt.plot(np.unique(xj), np.poly1d(np.polyfit(xj, yj, 1))(np.unique(xj)),line_choices[i],label=paths[i])
+	plt.ylim(0.0,1.9)
+	plt.plot(np.unique(xj), np.poly1d(np.polyfit(xj, yj, 1))(np.unique(xj)),line_choices[i])
 	plt.legend()
 	
 	i += 1
 
 plt.ylabel("White Noise Std Dev from 0.1V")
 plt.xlabel("Time to first event (ns)")
-plt.show()
+plt.savefig('../multiple_sources.png', dpi=dpi)
+plt.close()
